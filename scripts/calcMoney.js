7278efeef8ba65bfab7cc4e3ff3f960beb967ac9
MUKTADIR-ALAM <muktadiralam38@gmail.com>
2024-09-24 01:02:50 +0600

    add function and reduce code

--- scripts/calcMoney.js
+++ scripts/calcMoney.js
@@ -1,11 +1,27 @@
-document.getElementById('noakhali_btn').addEventListener('click', ()=>{
-    const donateAmtNoa = getInputFeidVlaueById('noakhali_input');
-    const noaTotalAmmout = getTextFeildVlaueById('noa-total-amount');
-    const waletMoney = getTextFeildVlaueById('walet-money');
-     if(donateAmtNoa <=0 || typeof donateAmtNoa !== 'number' || waletMoney < donateAmtNoa){
-      alert('Invali donation ammount');
-     }else{
-        document.getElementById('noa-total-amount').innerText = noaTotalAmmout + donateAmtNoa;
-        document.getElementById('walet-money').innerText = waletMoney - donateAmtNoa;
-     }
+// document.getElementById('noakhali_btn').addEventListener('click', ()=>{
+//     const donateAmtNoa = getInputFeidVlaueById('noakhali_input');
+//     const noaTotalAmmout = getTextFeildVlaueById('noa-total-amount');
+//     const waletMoney = getTextFeildVlaueById('walet-money');
+//      if(donateAmtNoa <= 0 || typeof donateAmtNoa !== 'number' || waletMoney < donateAmtNoa || isNaN(donateAmtNoa)){
+//          alert('Invali donation ammount');
+//         }else{
+//          document.getElementById('noa-total-amount').innerText = noaTotalAmmout + donateAmtNoa;
+//          document.getElementById('walet-money').innerText = waletMoney - donateAmtNoa;
+//          my_modal_5.showModal();
+
+//          document.getElementById('history').innerHTML += `<div class="border border-slate-200 p-8 rounded-xl">
+//           <h1 class="font-bold text-xl">${donateAmtNoa} Taka is Donated for famine-2024 at Feni, Bangladesh</h1>
+//           <p class="font-light text-base">Date : ${new Date()}</p>
+//         </div>`
+//      }
+// })
+
+document.getElementById('noakhali-btn').addEventListener('click', ()=>{
+    donation('noakhali-flood','noa-total-amount');
+})
+document.getElementById('feni-btn').addEventListener('click', ()=>{
+    donation('feni-flood','feni-total-amount');
+})
+document.getElementById('quota-btn').addEventListener('click', ()=>{
+    donation('quota-movement','quota-total-amount');
 })
--- scripts/function.js
+++ scripts/function.js
@@ -6,3 +6,22 @@ function getTextFeildVlaueById(id){
     return parseFloat(document.getElementById(id).innerText);
    
 }
+
+
+function donation (sectorInputFeild,sectorTotalAmmoutId) {
+        const donateAmt = getInputFeidVlaueById(sectorInputFeild);
+        const sectorTotalAmmout = getTextFeildVlaueById(sectorTotalAmmoutId);
+        const waletMoney = getTextFeildVlaueById('walet-money');
+         if(donateAmt <= 0 || typeof donateAmt !== 'number' || waletMoney < donateAmt || isNaN(donateAmt)){
+             alert('Invali donation ammount');
+            }else{
+             document.getElementById(sectorTotalAmmoutId).innerText = sectorTotalAmmout + donateAmt;
+             document.getElementById('walet-money').innerText = waletMoney - donateAmt;
+             my_modal_5.showModal();
+    
+             document.getElementById('history').innerHTML += `<div class="border border-slate-200 p-8 rounded-xl">
+              <h1 class="font-semibold text-sm lg:font-bold lg:text-xl">${donateAmt} Taka is Donated for famine-2024 at ${sectorInputFeild}, Bangladesh</h1>
+              <p class="font-light text-sm lg:font-light lg:text-base">Date : ${new Date()}</p>
+            </div>`
+         }
+    }
